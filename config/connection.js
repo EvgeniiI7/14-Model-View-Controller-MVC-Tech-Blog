183bb6a53f81df28214222c4983db5f979d85d24
Evgenii Ivanov <evgenii7.ivanov@yandex.ru>
2022-02-04 15:00:44 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,25 @@ require('dotenv').config();
 
 let sequelize;
 
+//if (process.env.JAWSDB_URL) {
+ //   sequelize = new Sequelize(process.env.JAWSDB_URL);
+//} else {
+ //   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+   //     host: 'localhost',
+//        dialect: 'mysql',
+//        port: 3306
+ //   });
+//}
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "password",
+      database: "tech_blog"
     });
-}
+  }
 
 module.exports = sequelize;
